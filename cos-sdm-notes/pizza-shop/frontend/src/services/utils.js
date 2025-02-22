2db9faeca169897bd9ef4cad92d574334d6da9f5
hackmeet <w.vasanimeet@gmail.com>
2025-02-22 11:13:34 +0530

    added pizza shop

--- cos-sdm-notes/pizza-shop/frontend/src/services/utils.js
+++
@@ -1,9 +0,0 @@
-import config from '../config'
-
-export function createUrl(path) {
-  return `${config.server}/${path}`
-}
-
-export function createError(error) {
-  return { status: 'error', error }
-}
